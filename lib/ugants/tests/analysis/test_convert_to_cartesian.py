3fa46e7a56199ae6ef9792aeac2a03adeda2f120
Josh Rackham <144251043+jrackham-mo@users.noreply.github.com>
2026-08-03 11:27:04 +0100

    Replace C4 data load in convert_to_cartesian tests

--- lib/ugants/tests/analysis/test_convert_to_cartesian.py
+++ lib/ugants/tests/analysis/test_convert_to_cartesian.py
@@ -8,8 +8,7 @@ import numpy as np
 import pytest
 from numpy.testing import assert_allclose
 from ugants.analysis.coord_transforms import convert_to_cartesian
-from ugants.io import load
-from ugants.tests import get_data_path
+from ugants.tests.stock import cubedsphere_cube
 
 pytestmark = pytest.mark.filterwarnings(
     "ignore:Assuming a spherical geocentric coordinate system for conversion to "
@@ -20,9 +19,7 @@ pytestmark = pytest.mark.filterwarnings(
 
 @pytest.fixture()
 def sample_data():
-    data_C4 = load.ugrid(get_data_path("data_C4.nc"))
-    sample_data = data_C4.extract_cube("sample_data")
-    return sample_data
+    return cubedsphere_cube(4)
 
 
 def test_radii(sample_data):
@@ -36,25 +33,20 @@ def test_individual_points(sample_data):
     """Check a handful of points in the sample data.
 
     >>> sample_data.coord("longitude").points[[1, 30, 42, 50, 87]]
-    masked_array(data=[348.62056535, 101.37943465, 191.27974683, 281.37943465,
-                       163.28214317],
-                 mask=False,
-                 fill_value=1e+20)
+    array([-143.13010235,   36.86989765,  -75.96375653,   75.96375653,
+           -18.43494882])
     >>> sample_data.coord("latitude").points[[1, 30, 42, 50, 87]]
-    masked_array(data=[ 33.30129185, -33.30129185, -11.06729582,  33.30129185,
-                       -55.02127549],
-                 mask=False,
-                 fill_value=1e+20)
+    array([-11.30993247, -11.30993247,  13.63302223,  36.03989343,
     """
     point_cloud = convert_to_cartesian(sample_data)
     actual_subset = point_cloud[[1, 30, 42, 50, 87]]
     expected_subset = np.array(
         [
-            [0.81936497, -0.16490693, 0.54904166],
-            [-0.16490693, 0.81936497, -0.54904166],
-            [-0.96244549, -0.19196182, -0.19196182],
-            [0.16490693, -0.81936497, 0.54904166],
-            [-0.54904166, 0.16490693, -0.81936497],
+            [-0.78446454, -0.58834841, -0.19611614],
+            [0.78446454, 0.58834841, -0.19611614],
+            [0.23570226, -0.94280904, 0.23570226],
+            [0.19611614, 0.78446454, 0.58834841],
+            [0.58834841, -0.19611614, 0.78446454],
         ]
     )
     np.testing.assert_array_almost_equal(actual_subset, expected_subset)
