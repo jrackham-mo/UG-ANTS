27572b01dd319ac4a02e6a2a2dc1bad93dc4886d
Josh Rackham <144251043+jrackham-mo@users.noreply.github.com>
2026-08-03 09:42:56 +0100

    Extract polydata_to_mesh to standalone function, and add mesh2cube util

--- lib/ugants/tests/_mesh.py
+++ lib/ugants/tests/_mesh.py
@@ -7,91 +7,27 @@
 import numpy as np
 import pyvista as pv
 from iris.coords import AuxCoord
-from iris.experimental.ugrid import Connectivity, Mesh, save_mesh
+from iris.experimental.ugrid import Connectivity, Mesh
 
 
 def cubedsphere_mesh(side_length):
     box_polydata = pv.Box(level=side_length - 1)
-    cell_centres_polydata = box_polydata.cell_centers()
-
-    node_lats, node_lons = calculate_lat_lon(box_polydata.points)
-    face_lats, face_lons = calculate_lat_lon(cell_centres_polydata.points)
-
-    face_node_indices = box_polydata.regular_faces
-    face_face_indices = np.array(
-        [box_polydata.cell_neighbors(i, "edges") for i in range(box_polydata.n_cells)]
-    )
-
-    node_x_aux = AuxCoord(
-        points=node_lons,
-        standard_name="longitude",
-        units="degrees_east",
-        long_name="node_x_coordinates",
-    )
-    node_y_aux = AuxCoord(
-        points=node_lats,
-        standard_name="latitude",
-        units="degrees_north",
-        long_name="node_y_coordinates",
-    )
-    face_x_aux = AuxCoord(
-        points=face_lons,
-        standard_name="longitude",
-        units="degrees_east",
-        long_name="face_x_coordinates",
-    )
-    face_y_aux = AuxCoord(
-        points=face_lats,
-        standard_name="latitude",
-        units="degrees_north",
-        long_name="face_y_coordinates",
-    )
-    face_node_connectivity = Connectivity(
-        indices=face_node_indices, cf_role="face_node_connectivity", start_index=0
-    )
-    face_face_connectivity = Connectivity(
-        indices=face_face_indices, cf_role="face_face_connectivity", start_index=0
-    )
-    mesh = Mesh(
-        long_name="my_mesh",
-        topology_dimension=2,
-        node_coords_and_axes=[(node_x_aux, "x"), (node_y_aux, "y")],
-        connectivities=[face_node_connectivity, face_face_connectivity],
-        face_coords_and_axes=[(face_x_aux, "x"), (face_y_aux, "y")],
-    )
+    mesh = _polydata_to_mesh(box_polydata)
     return mesh
 
 
-def panel_mesh(side_length, orientation):
-    n_faces = side_length * side_length
-    orientation_map = {
-        "+x": (1, 0, 0),
-        "-x": (-1, 0, 0),
-        "+y": (0, 1, 0),
-        "-y": (0, -1, 0),
-        "+z": (0, 0, 1),
-        "-z": (0, 0, -1),
-    }
-    direction = orientation_map[orientation]
-    plane_polydata = pv.Plane(
-        center=direction,
-        direction=direction,
-        i_size=2,
-        j_size=2,
-        i_resolution=side_length,
-        j_resolution=side_length,
-    )
+def _polydata_to_mesh(polydata: pv.PolyData):
+    cell_centres_polydata = polydata.cell_centers()
 
-    cell_centres_polydata = plane_polydata.cell_centers()
-
-    node_lats, node_lons = calculate_lat_lon(plane_polydata.points)
+    node_lats, node_lons = calculate_lat_lon(polydata.points)
     face_lats, face_lons = calculate_lat_lon(cell_centres_polydata.points)
 
-    face_node_indices = plane_polydata.regular_faces
+    face_node_indices = polydata.regular_faces
 
+    n_faces = polydata.n_cells
     face_face_indices = np.ma.masked_all((n_faces, 4), dtype=int)
     for face_id in range(n_faces):
-        connected_faces = plane_polydata.cell_neighbors(face_id, "edges")
+        connected_faces = polydata.cell_neighbors(face_id, "edges")
         face_face_indices[face_id, : len(connected_faces)] = connected_faces
 
     node_x_aux = AuxCoord(
@@ -134,6 +70,28 @@ def panel_mesh(side_length, orientation):
     return mesh
 
 
+def panel_mesh(side_length, orientation):
+    orientation_map = {
+        "+x": (1, 0, 0),
+        "-x": (-1, 0, 0),
+        "+y": (0, 1, 0),
+        "-y": (0, -1, 0),
+        "+z": (0, 0, 1),
+        "-z": (0, 0, -1),
+    }
+    direction = orientation_map[orientation]
+    plane_polydata = pv.Plane(
+        center=direction,
+        direction=direction,
+        i_size=2,
+        j_size=2,
+        i_resolution=side_length,
+        j_resolution=side_length,
+    )
+    mesh = _polydata_to_mesh(plane_polydata)
+    return mesh
+
+
 def calculate_lat_lon(points):
     x = points[:, 0]
     y = points[:, 1]
@@ -145,9 +103,32 @@ def calculate_lat_lon(points):
 
 
 if __name__ == "__main__":
-    import os
+    import argparse
     import pathlib
 
-    path = pathlib.Path(os.environ["SCRATCH"]) / "C12_mesh.nc"
-    mesh = cubedsphere_mesh(12)
-    save_mesh(mesh, path)
+    from ugants.io.save import ugrid
+    from ugants.utils.cube import mesh2cube
+
+    parser = argparse.ArgumentParser()
+
+    subparsers = parser.add_subparsers(dest="command", required=True)
+
+    cubedsphere_subparser = subparsers.add_parser("cubedsphere")
+    cubedsphere_subparser.add_argument("side_length", type=int)
+
+    panel_subparser = subparsers.add_parser("panel")
+    panel_subparser.add_argument("side_length", type=int)
+    panel_subparser.add_argument("orientation")
+
+    parser.add_argument("output", type=pathlib.Path)
+
+    args = parser.parse_args()
+    print(args)
+    match args.command:
+        case "cubedsphere":
+            mesh = cubedsphere_mesh(args.side_length)
+        case "panel":
+            mesh = panel_mesh(args.side_length, args.orientation)
+
+    cube = mesh2cube(mesh)
+    ugrid(cube, args.output)
